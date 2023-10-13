a3da1110b9a40e36111d75da255365cb6fbc8904
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-13 23:13:40 +0700

    Implement GET, POST, DELETE operations on cart endpoint

--- store/views.py
+++ store/views.py
@@ -2,13 +2,14 @@ from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .filters import ProductFilter
-from .models import Product, Collection, OrderItem, Review
+from .models import Product, Collection, OrderItem, Review, Cart
 from .pagination import DefaultPagination
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -55,3 +56,13 @@ class ReviewViewSet(ModelViewSet):
 
 	def get_serializer_context(self):
 		return {"product_id": self.kwargs["product_pk"]}
+
+
+class CartViewSet(
+	CreateModelMixin,
+	RetrieveModelMixin,
+	DestroyModelMixin,
+	GenericViewSet
+):
+	queryset = Cart.objects.prefetch_related("items__product").all()
+	serializer_class = CartSerializer
