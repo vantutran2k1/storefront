2e5811529e819338b29a48e5897a554d99e8dd72
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-13 21:13:57 +0700

    Create basic nested Reviews API

--- store/views.py
+++ store/views.py
@@ -3,8 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Product, Collection, OrderItem
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Product, Collection, OrderItem, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -36,3 +36,13 @@ class CollectionViewSet(ModelViewSet):
 			)
 
 		return super().destroy(request, *args, **kwargs)
+
+
+class ReviewViewSet(ModelViewSet):
+	serializer_class = ReviewSerializer
+
+	def get_queryset(self):
+		return Review.objects.filter(product_id=self.kwargs["product_pk"])
+
+	def get_serializer_context(self):
+		return {"product_id": self.kwargs["product_pk"]}
