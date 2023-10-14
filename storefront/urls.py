40aa6adbfdfe71cf4ccd2f28b84e56e85c258b3c
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-14 16:52:00 +0700

    Set up Djoser for authentication

--- storefront/urls.py
+++ storefront/urls.py
@@ -24,5 +24,7 @@ admin.site.index_title = "Admin"
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('__debug__/', include(debug_toolbar.urls)),
-	path('store/', include('store.urls'))
+	path('store/', include('store.urls')),
+	path('auth/', include('djoser.urls')),
+	path('auth/', include('djoser.urls.jwt'))
 ]
