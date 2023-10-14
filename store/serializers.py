1810e725748be14170eb4a6f8e9e3e18786d5bae
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-15 00:13:46 +0700

    Get custom signal for order created

--- store/serializers.py
+++ store/serializers.py
@@ -4,6 +4,7 @@ from django.db import transaction
 from rest_framework import serializers
 
 from .models import Product, Collection, Review, Cart, CartItem, Customer, Order, OrderItem
+from .signals import order_created
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -160,5 +161,7 @@ class CreateOrderSerializer(serializers.Serializer):
 			OrderItem.objects.bulk_create(order_items)
 
 			Cart.objects.filter(pk=cart_id).delete()
+
+			order_created.send_robust(self.__class__, order=order)
 
 		return order
