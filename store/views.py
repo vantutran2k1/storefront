78dc41ce7abcc48182c57cdc71a3a4f500c3b740
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-14 22:36:11 +0700

    Implement GET request method of orders api

--- store/views.py
+++ store/views.py
@@ -9,11 +9,11 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .filters import ProductFilter
-from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer, Order
 from .pagination import DefaultPagination
 from .permissions import IsAdminOrReadOnly, ViewCustomerHistoryPermission
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, \
-	AddCartItemSerializer, UpdateCartItemSerializer, CustomerSerializer
+	AddCartItemSerializer, UpdateCartItemSerializer, CustomerSerializer, OrderSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -112,3 +112,17 @@ class CustomerViewSet(ModelViewSet):
 			serializer.is_valid(raise_exception=True)
 			serializer.save()
 			return Response(serializer.data)
+
+
+class OrderViewSet(ModelViewSet):
+	serializer_class = OrderSerializer
+	permission_classes = [IsAuthenticated]
+
+	def get_queryset(self):
+		user = self.request.user
+
+		if user.is_staff:
+			return Order.objects.all()
+
+		(customer_id, created) = Customer.objects.only("id").get_or_create(user_id=user.id)
+		return Order.objects.filter(customer_id=customer_id)
