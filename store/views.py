2f1ed2a54385d069203406ae27d6b10ce5e0d628
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-14 19:13:30 +0700

    Get current customer after authentication

--- store/views.py
+++ store/views.py
@@ -1,16 +1,17 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .filters import ProductFilter
-from .models import Product, Collection, OrderItem, Review, Cart, CartItem
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer
 from .pagination import DefaultPagination
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, \
-	AddCartItemSerializer, UpdateCartItemSerializer
+	AddCartItemSerializer, UpdateCartItemSerializer, CustomerSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -84,3 +85,21 @@ class CartItemViewSet(ModelViewSet):
 
 	def get_serializer_context(self):
 		return {"cart_id": self.kwargs["cart_pk"]}
+
+
+class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+	queryset = Customer.objects.all()
+	serializer_class = CustomerSerializer
+
+	@action(detail=False, methods=["GET", "PUT"])
+	def me(self, request):
+		customer, created = Customer.objects.get_or_create(user_id=request.user.id)
+
+		if request.method == "GET":
+			serializer = CustomerSerializer(customer)
+			return Response(serializer.data)
+		elif request.method == "PUT":
+			serializer = CustomerSerializer(customer, data=request.data)
+			serializer.is_valid(raise_exception=True)
+			serializer.save()
+			return Response(serializer.data)
--- storefront/settings.py
+++ storefront/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,6 +141,14 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
 	'AUTH_HEADER_TYPES': ('JWT',),
+	'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
 
 AUTH_USER_MODEL = 'core.User'
+
+DJOSER = {
+	'SERIALIZERS': {
+		'user_create': 'core.serializers.UserCreateSerializer',
+		'current_user': 'core.serializers.UserSerializer'
+	}
+}
