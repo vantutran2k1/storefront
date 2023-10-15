1bfe46f3c167d4558f6ec009e6b2da529d0592fe
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-15 12:43:53 +0700

    Retrieve product images to client

--- store/views.py
+++ store/views.py
@@ -18,7 +18,7 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 
 
 class ProductViewSet(ModelViewSet):
-	queryset = Product.objects.all()
+	queryset = Product.objects.prefetch_related("images").all()
 	serializer_class = ProductSerializer
 	filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
 	filterset_class = ProductFilter
