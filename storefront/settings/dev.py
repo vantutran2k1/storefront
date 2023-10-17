8ad6d96c4b49ffa6dc0dcc37c953c1206c811212
Van Tu Tran <vantu.tran2k1@gmail.com>
2023-10-17 10:17:35 +0700

    Dockerize app

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -13,7 +13,7 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'storefront',
-		'HOST': 'localhost',
+		'HOST': 'mysql',
 		'USER': 'root',
 		'PASSWORD': 'root'
 	}
@@ -21,18 +21,22 @@ DATABASES = {
 
 MIDDLEWARE += ['silk.middleware.SilkyMiddleware']
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 CACHES = {
 	'default': {
 		'BACKEND': 'django_redis.cache.RedisCache',
-		'LOCATION': 'redis://127.0.0.1:6379/2',
+		'LOCATION': 'redis://redis:6379/2',
 		'OPTIONS': {
 			'CLIENT_CLASS': 'django_redis.client.DefaultClient',
 		}
 	}
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+	'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
