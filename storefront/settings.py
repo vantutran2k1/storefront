8cc1dc19c98834a175dcd6e454a8c5bed3955113
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-03-31 23:56:38 +0700

    Setup MySql database and create seed data

--- storefront/settings.py
+++ storefront/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'MyPassword'
     }
 }
 
