f4ad688053c478d583bfc2b283d65a430d537b77
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-04-03 23:36:06 +0700

    Add sorting and pagination features

--- store/views.py
+++ store/views.py
@@ -1,21 +1,24 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from store.filters import ProductFilter
 from store.models import Product, Collection, OrderItem, Review
+from store.pagination import DefaultPagination
 from store.serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
