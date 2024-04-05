2c0051e4900ddb398590a9ae8d5e2ac0327aafd0
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-04-05 09:23:26 +0700

    Configure djoser for user authentication

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'debug_toolbar',
     'store',
     'tags',
@@ -131,7 +132,14 @@ INTERNAL_IPS = [
 ]
 
 REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING': False
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',)
 }
 
 AUTH_USER_MODEL = 'core.User'
