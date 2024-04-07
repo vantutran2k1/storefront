6e66a3a6b5fb58ddd44c00fdb4abc229146e4fbc
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-04-07 14:51:15 +0700

    Add the home page

--- storefront/urls.py
+++ storefront/urls.py
@@ -24,6 +24,7 @@ admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
